f7efca092fbce3bd14487de093a0721f46c19155
HueyPark <jaewan.huey.park@gmail.com>
2015-10-21 23:40:46 +0900

    Add slide.js

--- app/app.js
+++ app/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var slides = require('./routes/slides');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/slides', slides);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next)
---
+++ app/routes/slides.js
@@ -0,0 +1,9 @@
+﻿var express = require('express');
+var router = express.Router();
+
+router.get('/:filename', function (req, res, next)
+{
+    res.render('slide', { title: req.params.filename });
+});
+
+module.exports = router;
