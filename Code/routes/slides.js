063aa6bc7c93ba2efa96dfbb965a357c79b8bd89
HueyPark <jaewan.huey.park@gmail.com>
2015-10-31 14:31:21 +0900

    Update: Handling slide filename

--- Code/routes/slides.js
+++ Code/routes/slides.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 router.get('/:filename', function (req, res, next)
 {
-    res.render('slide', { title: req.params.filename });
+    res.render('slide', { markdown: req.params.filename });
 });
 
 module.exports = router;
