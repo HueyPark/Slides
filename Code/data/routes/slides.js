ab528dd5a330ee06f323aca3c0c67b84ade25a87
hueypark <jaewan.huey.park@gmail.com>
2016-02-22 20:31:35 +0900

    Delete: legacy files

--- Code/data/routes/slides.js
+++
@@ -1,9 +0,0 @@
-﻿var express = require('express');
-var router = express.Router();
-
-router.get('/:filename', function (req, res, next)
-{
-    res.render('slide', { markdown: req.params.filename });
-});
-
-module.exports = router;
